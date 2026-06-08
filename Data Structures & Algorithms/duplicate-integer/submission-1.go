2e5dc28abc9e5e58de8546192e80d3232ad8d955
mossprune <250817288+mossprune@users.noreply.github.com>
2026-06-08 22:26:01 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,11 @@
+func hasDuplicate(nums []int) bool {
+    set := make(map[int]struct{})
+
+    for _, n := range(nums) {
+        if _, exists := (set[n]); exists{
+            return true
+        }
+        set[n] = struct{}{}
+    }
+    return false
+}
